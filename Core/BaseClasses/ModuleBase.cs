8b2236d9319bbeed9c8d97d50962731cd80464d7
Quinten De Clerck <quinten.de.clerck@outlook.com>
2023-12-17 19:31:15 +0100

    [FIX] project references

---
+++ Core/BaseClasses/ModuleBase.cs
@@ -0,0 +1,13 @@
+﻿using Core.Interfaces;
+using System.Reflection;
+
+namespace Core.BaseClasses
+{
+    public abstract class ModuleBase : IModule
+    {
+        public Assembly GetAssembly()
+        {
+            return Assembly.GetExecutingAssembly(); // return assembly
+        }
+    }
+}
