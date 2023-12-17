6d36062c2da7ac9c398f198ad690c122cf75f326
Quinten De Clerck <quinten.de.clerck@outlook.com>
2023-12-17 17:51:12 +0100

    [WIP]

--- BlazorAppWithRcl/Program.cs
+++ BlazorAppWithRcl/Program.cs
@@ -1,6 +1,8 @@
 using BlazorAppWithRcl.Client.Pages;
 using BlazorAppWithRcl.Components;
+using Core;
 using Module1;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -31,10 +33,40 @@ app.UseAntiforgery();
 // check https://github.com/dotnet/aspnetcore/issues/48767, to fix issue
 // https://github.com/treefishuk/ModularBlazor/blob/master/src/ModularBlazor.App/Startup.cs
 
+var assemblies = GetAssembliesWithModule<IModule>();
+
+//app.MapRazorComponents<App>()
+//    .AddInteractiveServerRenderMode()
+//    .AddInteractiveWebAssemblyRenderMode()
+//    .AddAdditionalAssemblies(typeof(Counter).Assembly)
+//    .AddAdditionalAssemblies(typeof(Test).Assembly);
+
+
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode()
     .AddInteractiveWebAssemblyRenderMode()
-    .AddAdditionalAssemblies(typeof(Counter).Assembly)
-    .AddAdditionalAssemblies(typeof(Test).Assembly);
+    .AddAdditionalAssemblies(assemblies);
 
 app.Run();
+
+
+static Assembly[] GetAssembliesWithModule<T>() where T : IModule
+{
+    var assembliesWithModule = AppDomain.CurrentDomain.GetAssemblies()
+        .SelectMany(assembly => assembly.GetTypes())
+        .Where(type => typeof(T).IsAssignableFrom(type) && type.IsClass)
+        .Select(type =>
+        {
+            // Check of de klasse een default constructor heeft
+            if (type.GetConstructor(Type.EmptyTypes) != null)
+            {
+                var moduleInstance = (T)Activator.CreateInstance(type);
+                return moduleInstance.GetModuleAssembly();
+            }
+            return null;
+        })
+        .Where(assembly => assembly != null)
+        .ToArray();
+
+    return assembliesWithModule;
+}
---
+++ Module1/Module1.cs
@@ -0,0 +1,20 @@
+﻿using Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module1
+{
+    public class Module1 : IModule
+    {
+        // Voeg hier je specifieke implementatie toe
+        public Assembly GetModuleAssembly()
+        {
+            return typeof(Module1).Assembly;
+        }
+    }
+
+}
