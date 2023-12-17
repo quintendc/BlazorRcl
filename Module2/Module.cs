361ba66cafc27a420f00e54b3ac7fb5fd1c1a6b6
Quinten De Clerck <quinten.de.clerck@outlook.com>
2023-12-17 19:15:27 +0100

    [ADD] module 2

---
+++ Module2/Module.cs
@@ -0,0 +1,13 @@
+﻿using Core.BaseClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module2
+{
+    public class Module : ModuleBase
+    {
+    }
+}
